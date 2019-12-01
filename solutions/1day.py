8436e6cd1e2a9e86a7cf105ec361cbc47a736059
jpclark6 <jpclark6@gmail.com>
2019-12-01 16:13:18 -0700

    added elapsed time calculations

--- solutions/1day.py
+++ solutions/1day.py
@@ -1,4 +1,7 @@
 import math
+import time
+
+start = time.time()
 
 def find_fuel(mass):
     return math.floor(mass / 3) - 2
@@ -14,6 +17,9 @@ for mass in input():
 
 print("Total part 1:", total)
 
+mid = time.time()
+
+# Day 2
 total = 0
 for mass in input():
     current_fuel = find_fuel(mass)
@@ -22,3 +28,8 @@ for mass in input():
         current_fuel = find_fuel(current_fuel)
 
 print("Total part 2:", total)
+
+end = time.time()
+print("Elapsed time part 1:\t", round(mid - start, 6), "seconds")
+print("Elapsed time part 2:\t", round(end - mid, 6), "seconds")
+print("Elapsed time total:\t", round(end - start, 6), "seconds")
