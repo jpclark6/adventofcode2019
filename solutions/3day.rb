09f45b8cd9a78b87705379d9aed12ac340a6313f
Justin Clark <jpclark6@gmail.com>
2019-12-05 15:57:10 -0700

    refactored

--- solutions/3day.rb
+++ solutions/3day.rb
@@ -12,118 +12,81 @@ def instructions
     inst
 end
 
-def part_1
-    all_coords = [Set.new(), Set.new()]
-    current_x = 0
-    current_y = 0
-    instructions.each_with_index do |wire, j|
-        wire.each_with_index do |length, i|
-            if i == 0
-                current_x = 0
-                current_y = 0
-            end
-            case length[:direction]
-            when "R"
-                (current_x..current_x + length[:length]).each do |l|
-                    all_coords[j].add([l, current_y])
-                end
-                current_x += length[:length]
-            when "L"
-                current_x.downto(current_x - length[:length]).each do |l|
-                    all_coords[j].add([l, current_y])
-                end
-                current_x -= length[:length]
-            when "U"
-                (current_y..current_y + length[:length]).each do |l|
-                    all_coords[j].add([current_x, l])
-                end
-                current_y += length[:length]
-            when "D"
-                current_y.downto(current_y - length[:length]).each do |l|
-                    all_coords[j].add([current_x, l])
-                end
-                current_y -= length[:length]
-            end
-        end
-    end
-    
-    pairs = (all_coords[0].intersection all_coords[1]).map do |point|
-        manhattan(point)
-    end
-    sorted = pairs.sort
-    puts "Answer to part 1:", sorted[1]
-end
-
-def part_2
-    all_coords = [Hash.new(), Hash.new()]
-    current_x = 0
-    current_y = 0
-    tot_distance = 0
-    instructions.each_with_index do |wire, j|
-        wire.each_with_index do |length, i|
-            if i == 0
-                current_x = 0
-                current_y = 0
-                tot_distance = 0
-            end
-            case length[:direction]
-            when "R"
-                (current_x..current_x + length[:length]).each do |l|
-                    if all_coords[j][[l, current_y]] == nil
-                        all_coords[j][[l, current_y]] = tot_distance
-                    end
-                    tot_distance += 1
-                end
-                current_x += length[:length]
-            when "L"
-                current_x.downto(current_x - length[:length]).each do |l|
-                    if all_coords[j][[l, current_y]] == nil
-                        all_coords[j][[l, current_y]] = tot_distance
-                    end
-                    tot_distance += 1
-                end
-                current_x -= length[:length]
-            when "U"
-                (current_y..current_y + length[:length]).each do |l|
-                    if all_coords[j][[current_x, l]] == nil
-                        all_coords[j][[current_x, l]] = tot_distance
-                    end
-                    tot_distance += 1
-                end
-                current_y += length[:length]
-            when "D"
-                current_y.downto(current_y - length[:length]).each do |l|
-                    if all_coords[j][[current_x, l]] == nil
-                        all_coords[j][[current_x, l]] = tot_distance
-                    end
-                    tot_distance += 1
-                end
-                current_y -= length[:length]
-            end
-            tot_distance -= 1
-        end
-    end
-    
-    coords = [Set.new(), Set.new()]
-    (0..1).each do |i|
-        all_coords[i].keys.each do |key|
-            coords[i].add(key)
-        end
-    end
-
-    steps_to_intersection = []
-    (coords[0] & coords[1]).each do |coord|
-        steps_to_intersection << all_coords[0][coord] + all_coords[1][coord] 
-    end
-    sorted = steps_to_intersection.sort
-    puts "Answer to part 2:", sorted[1]
-end
-
-def manhattan(point)
+def find_manhattan_dist(point)
     x = point[0]
     y = point[1]
     x.abs + y.abs
 end
 
-part_1
-part_2
+def first_time_to_location?(loc)
+    loc == nil
+end
+
+def solution
+    wires_coords = [Hash.new(), Hash.new()]
+    current_x, current_y, tot_distance = 0, 0, 0
+    
+    instructions.each_with_index do |bend, wire_number|
+        bend.each_with_index do |length, bend_number|
+            if bend_number == 0
+                current_x, current_y, tot_distance = 0, 0, 0
+            end
+            dist = length[:length]
+            
+            case length[:direction]
+            when "R"
+                (current_x..current_x + dist).each do |coord|
+                    if first_time_to_location?(wires_coords[wire_number][[coord, current_y]])
+                        wires_coords[wire_number][[coord, current_y]] = tot_distance
+                    end
+                    tot_distance += 1
+                end
+                current_x += dist
+            when "L"
+                current_x.downto(current_x - dist).each do |coord|
+                    if first_time_to_location?(wires_coords[wire_number][[coord, current_y]])
+                        wires_coords[wire_number][[coord, current_y]] = tot_distance
+                    end
+                    tot_distance += 1
+                end
+                current_x -= dist
+            when "U"
+                (current_y..current_y + dist).each do |coord|
+                    if first_time_to_location?(wires_coords[wire_number][[current_x, coord]])
+                        wires_coords[wire_number][[current_x, coord]] = tot_distance
+                    end
+                    tot_distance += 1
+                end
+                current_y += dist
+            when "D"
+                current_y.downto(current_y - dist).each do |coord|
+                    if first_time_to_location?(wires_coords[wire_number][[current_x, coord]])
+                        wires_coords[wire_number][[current_x, coord]] = tot_distance
+                    end
+                    tot_distance += 1
+                end
+                current_y -= dist
+            end
+            tot_distance -= 1
+        end
+    end
+
+    # part 1
+    intersections = wires_coords[0].keys & wires_coords[1].keys
+    intersection_distances = intersections.map do |intersecting_point|
+        find_manhattan_dist(intersecting_point)
+    end
+    sorted = intersection_distances.sort
+    sorted.shift # remove 0, 0 coordinate
+    puts "Answer to part 1:", sorted.first
+
+    # part 2
+    steps_to_intersection = []
+    steps_to_intersection = intersections.map do |coord|
+        wires_coords[0][coord] + wires_coords[1][coord] 
+    end
+    sorted = steps_to_intersection.sort
+    puts "Answer to part 2:", sorted[1]
+end
+
+solution
