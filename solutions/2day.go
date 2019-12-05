20bac7fc0a30d5c258b696b1b0cc20360cb660c4
jpclark6 <jpclark6@gmail.com>
2019-12-04 19:22:47 -0700

    solved part 2 brute force but still runs in 89 ms

--- solutions/2day.go
+++ solutions/2day.go
@@ -10,8 +10,14 @@ import (
 )
 
 func main() {
+	startTime := time.Now()
 	part1()
+	midTime := time.Now()
 	part2()
+	endTime := time.Now()
+	fmt.Println("Elapsed time for part 1:", midTime.Sub(startTime))
+	fmt.Println("Elapsed time for part 2:", endTime.Sub(midTime))
+	fmt.Println("Elapsed time for both parts:", endTime.Sub(startTime))
 }
 
 func getProgram() []int {
@@ -30,12 +36,11 @@ func getProgram() []int {
 	return program
 }
 
-func part1() {
-	startTime := time.Now()
+func fixProgram(noun, verb int) int {
 	program := getProgram()
 
-	program[1] = 12
-	program[2] = 2
+	program[1] = noun
+	program[2] = verb
 
 	i := 0
 
@@ -54,12 +59,24 @@ Loop:
 		}
 		i += 4
 	}
-	fmt.Println("Solution to part 1:", program[0])
+	return program[0]
+}
 
-	end := time.Now()
-	fmt.Println("Elapsed time for part 1:", end.Sub(startTime))
+func part1() {
+	ans := fixProgram(12, 2)
+	fmt.Println("Solution to part 1:", ans)
 }
 
 func part2() {
-	fmt.Println("Still works")
+	expectedOutput := 19690720
+Loop:
+	for noun := 0; noun < 100; noun++ {
+		for verb := 0; verb < 100; verb++ {
+			output := fixProgram(noun, verb)
+			if output == expectedOutput {
+				fmt.Println("Answer to part 2:", 100 * noun + verb)
+				break Loop
+			}
+		}
+	}
 }
