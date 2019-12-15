f056075474c51fa388153b98a6692630e6e41d8f
Justin Clark <jpclark6@gmail.com>
2019-12-15 15:59:04 -0700

    part 1 day 14 finished

--- solutions/14day.go
+++ solutions/14day.go
@@ -3,15 +3,14 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	// "math"
 	"strconv"
 	"strings"
-	// "time"
 )
 
 func main() {
 	cookbook := parseFile("./puzzledata/14day.txt")
-	fmt.Println(cookbook)
+	ore := findOre("FUEL", cookbook)
+	fmt.Println("Total ore required:", ore)
 }
 
 type recipe struct {
@@ -23,6 +22,65 @@ type ingredient struct {
 	amount int
 	name   string
 }
+
+func findOre(output string, cookbook []recipe) (totalOre int){
+	required := make(map[string]int)
+	product := getProduct(output, cookbook)
+	required[product.name] = product.amount
+
+	for iterations := 1; ; iterations++{
+		for requiredItem, requiredAmount := range(required) {
+			if requiredAmount > 0 && requiredItem != "ORE" {
+				product = getProduct(requiredItem, cookbook)
+				newRequired := getRecipe(product, cookbook)
+				for i := 0; i < len(newRequired); i++ {
+					nR := newRequired[i]
+					nRAmount := required[nR.name]
+					nRAmount += nR.amount
+					required[nR.name] = nRAmount
+				}
+				required[product.name] -= product.amount
+			}
+		}
+		finished := true
+		for k, v := range(required) {
+			if k != "ORE" && v > 0 {
+				finished = false
+			}
+		}
+		if finished {
+			break
+		}
+	}
+	return required["ORE"]
+}
+
+func getExtra(output ingredient, extras []ingredient) (ingredient, bool) {
+	for i := 0; i < len(extras); i++ {
+		if extras[i].name == output.name {
+			return extras[i], true
+		}
+	}
+	return ingredient{}, false
+}
+
+func getProduct(output string, cookbook []recipe) ingredient {
+	for i := 0; i < len(cookbook); i++ {
+		if output == cookbook[i].output.name {
+			return cookbook[i].output
+		}
+	}
+	return ingredient{}
+}
+
+func getRecipe(output ingredient, cookbook []recipe) []ingredient {
+	for i := 0; i < len(cookbook); i++ {
+		if output.name == cookbook[i].output.name {
+			return cookbook[i].ingredients
+		}
+	}
+	return []ingredient{}
+} 
 
 func parseFile(file string) (cookbook []recipe) {
 	content, _ := ioutil.ReadFile(file)
