bdf7afd26efdd016cf77284b0ca4d3b23813ce42
Justin Clark <jpclark6@gmail.com>
2019-12-20 10:55:40 -0700

    parser finished

--- solutions/20day.py
+++ solutions/20day.py
@@ -1,15 +1,33 @@
+import re
+
 class Puzzle:
     def __init__(self, file_name):
-        self.tiles = []
+        self.tiles = {}
         file = open(file_name).read().splitlines()
         matrix = [[x for x in line] for line in file]
-        for y in range(2, len(matrix) - 2):
-            for x in range(2, len(matrix[0]) - 2):
-                self.tiles.append(Tile(x, y, matrix[y][x]))
+        for y in range(len(matrix)):
+            for x in range(len(matrix[0])):
+                if bool(re.search('[#.]', matrix[y][x])):
+                    tile = Tile(x, y, matrix[y][x])
+                    self.tiles[str(x) + "," + str(y)] = tile
+                    if bool(re.search('[A-Z]', matrix[y - 1][x])):
+                        tile.portal = True
+                        tile.portal_key = matrix[y - 2][x] + matrix[y - 1][x]
+                    elif bool(re.search('[A-Z]', matrix[y + 1][x])):
+                        tile.portal = True
+                        tile.portal_key = matrix[y + 1][x] + matrix[y + 2][x]
+                    elif bool(re.search('[A-Z]', matrix[y][x - 1])):
+                        tile.portal = True
+                        tile.portal_key = matrix[y][x - 2] + matrix[y][x - 1]
+                    elif bool(re.search('[A-Z]', matrix[y][x + 1])):
+                        tile.portal = True
+                        tile.portal_key = matrix[y][x + 1] + matrix[y][x + 2]
+
         import pdb; pdb.set_trace()
 
     def get_tile(self, x, y):
-        
+        key = str(x) + "," + str(y)
+        return self.tiles[key]
         
 
 class Tile:
